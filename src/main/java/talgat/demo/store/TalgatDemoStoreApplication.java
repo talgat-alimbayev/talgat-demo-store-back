dbe5b9107e791689bb948e06b2edf24a71898c37
talimbayev <alimbayev.talgat@gmail.com>
2022-12-19 20:23:04 +0600

    first commit

---
+++ src/main/java/talgat/demo/store/TalgatDemoStoreApplication.java
@@ -0,0 +1,13 @@
+package talgat.demo.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalgatDemoStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalgatDemoStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/talgat/demo/store/TalgatDemoStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package talgat.demo.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalgatDemoStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
